ece342241d4864201105e9290f128756e4b34865
jony <jony@jonydeMacBook-Air.local>
2017-06-28 10:01:24 +0800

    a

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        // 可对应多个入口文件
+        app: ['./src/js/index.js']
+    },
+    devtool: 'source-map', // 输出source-map
+    output:{
+        path:path.resolve(__dirname, './build/'),
+        filename:'app.js'
+    },
+    devServer: {
+        contentBase: './',
+        inline: true, // 项目文件保存自动编译文件模块
+        port: 8080
+    }, 
+    module: {
+      loaders: [
+        {test: /\.css$/, loader: 'style-loader!css-loader'},
+        {test: /\.jsx?$/, loader: 'babel-loader', query: {presets: ['es2015', 'react', 'stage-1']}}
+      ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'aaaa',
+            template: 'index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
